b2286f342c185bf1c8691e421c7abcf990e1bb08
divi1995-eg <divysharma@expediagroup.com>
2022-06-30 10:37:23 +0530

    Initial Commit

---
+++ Enthusiastic Wanderers/Enthusiastic_WanderersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enthusiastic_WanderersApp.swift
+//  Enthusiastic Wanderers
+//
+//  Created by Divyanshu Sharma on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Enthusiastic_WanderersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
